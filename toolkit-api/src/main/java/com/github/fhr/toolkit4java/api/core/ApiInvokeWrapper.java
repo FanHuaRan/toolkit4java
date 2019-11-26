9e1c5ba884558129a7c77148767f428578e6138a
huaran.fan <834171100@qq.com>
2019-11-26 23:55:15 +0800

    add api

---
+++ toolkit-api/src/main/java/com/github/fhr/toolkit4java/api/core/ApiInvokeWrapper.java
@@ -0,0 +1,16 @@
+package com.github.fhr.toolkit4java.api.core;
+
+public class ApiInvokeWrapper {
+
+    public void invoke(){
+        // 打日志
+
+        // 限流
+
+        // 人为降级
+
+        // 失败降级
+
+        // 失败回调
+    }
+}
